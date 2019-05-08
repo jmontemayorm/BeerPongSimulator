f92c8fd1ab4005477060a281fe878d6d06b9b15d
jmontemayorm <j.montemayor.mm@gmail.com>
2019-05-08 15:14:37 -0500

    Update shootBall

--- shootBall.m
+++ shootBall.m
@@ -1,6 +1,11 @@
-function OUTPUTVARIABLE = shootBall(neuralNetwork,environment)
+function court = shootBall(neuralNetwork,environment,court,player)
     % TODO: Add description
     
-    
+    % Get shooting angles and speed
+    if player == 1
+        inData = court.cups.status.player2;
+    else
+        inData = court.cups.status.player1;
+    end
 end
 
