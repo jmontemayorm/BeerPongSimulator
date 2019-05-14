a9466dde18c004a08d1802f4df110e83a89d26e0
jmontemayorm <j.montemayor.mm@gmail.com>
2019-05-14 09:08:57 -0500

    Changes to shooting zones

--- Court/getShootingZones.m
+++ Court/getShootingZones.m
@@ -50,12 +50,12 @@ function court = getShootingZones(court)
     court.shootingZones.player2.vertices(:,1) = -court.shootingZones.player2.vertices(:,1);
     
     % Add calculated data
-    court.shootingZones.absMinX = min(vertices(:,1));
-    court.shootingZones.absMaxX = max(vertices(:,1));
-    court.shootingZones.absMinY = min(vertices(:,2));
-    court.shootingZones.absMaxY = max(vertices(:,2));
-    court.shootingZones.absMinZ = min(vertices(:,3));
-    court.shootingZones.absMaxZ = max(vertices(:,3));
+    court.shootingZones.minX = min(abs(vertices(:,1)));
+    court.shootingZones.maxX = max(abs(vertices(:,1)));
+    court.shootingZones.minY = min(vertices(:,2));
+    court.shootingZones.maxY = max(vertices(:,2));
+    court.shootingZones.minZ = min(vertices(:,3));
+    court.shootingZones.maxZ = max(vertices(:,3));
     
     % Add to the figure
     figure(court.figure)
