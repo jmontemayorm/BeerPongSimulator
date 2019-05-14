c3dd6389dd8fa7a0928b981d0fbecd9fcda5ae15
jmontemayorm <j.montemayor.mm@gmail.com>
2019-05-14 09:13:35 -0500

    Refactored and completed code

--- createBall.m
+++ createBall.m
@@ -1,24 +1,42 @@
-function ball = createBall(p0,az,el,v0,player)
+function ball = createBall(networkOutput,court,player)
     % CREATEBALL takes an initial position vector, azimuthal and elevation
     % angles in radians, initial velocity, and player number and returns a
     % ball struct with position and speed.
+    % TODO: Fix description
+    
+    % Shooting ranges
+    minEl = deg2rad(10);
+    maxEl = pi/3;
+    maxAz = pi/12;
+    minVel = 4;
+    maxVel = 5;
     
     % Initialize ball as struct
     ball = struct;
     
     % Add initial position
-    ball.PosX = p0(1);
-    ball.PosY = p0(2);
-    ball.PosZ = p0(3);
+    ball.posX = networkOutput(1) .* court.shootingZones.xSize + court.shootingZones.minX;
+    if player == 2
+        ball.posX = -ball.posX;
+    end
+    ball.posY = (networkOutput(2) - 0.5) .* court.shootingZones.ySize;
+    ball.posZ = networkOutput(3) .* court.shootingZones.zSize + court.shootingZones.minZ;
     
-    % Mirror azimuthal angle for player 2
+    % Azimuthal angle
+    az = maxAz * (networkOutput(4) - 0.5);
     if player == 2
         az = az + pi;
     end
     
+    % Elevation angle
+    el = (maxEl - minEl) * networkOutput(5) + minEl;
+    
+    % Initial speed
+    v0 = (maxVel - minVel) * networkOutput(6) + minVel;
+    
     % Transform angles and velocity into speed
     vXY = v0 * cos(el);
-    ball.VelX = vXY * cos(az);
-    ball.VelY = vXY * sin(az);
-    ball.VelZ = v0 * sin(el);
+    ball.velX = vXY * cos(az);
+    ball.velY = vXY * sin(az);
+    ball.velZ = v0 * sin(el);
 end
