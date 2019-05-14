4dc758c65b2adf992ae42e69895f662b141352ac
jmontemayorm <j.montemayor.mm@gmail.com>
2019-05-14 10:49:57 -0500

    Added graphics for the ball

--- createBall.m
+++ createBall.m
@@ -14,6 +14,18 @@ function ball = createBall(networkOutput,court,player)
     % Initialize ball as struct
     ball = struct;
     
+    % Create mesh
+    ball.sphereVertices = 32;
+    ball.radius = 0.02;
+    
+    [ball.meshX,ball.meshY,ball.meshZ] = sphere(ball.sphereVertices - 1);
+    ball.meshX = ball.meshX .* ball.radius;
+    ball.meshY = ball.meshY .* ball.radius;
+    ball.meshZ = ball.meshZ .* ball.radius;
+    
+    ball.color = [0.95 0.95 0.95];
+    ball.edgeAlpha = 0.1;
+    
     % Add initial position
     ball.posX = networkOutput(1) .* court.shootingZones.xSize + court.shootingZones.minX;
     if player == 2
@@ -22,9 +34,18 @@ function ball = createBall(networkOutput,court,player)
     ball.posY = (networkOutput(2) - 0.5) .* court.shootingZones.ySize;
     ball.posZ = networkOutput(3) .* court.shootingZones.zSize + court.shootingZones.minZ;
     
+    % Update mesh
+    ball.baseMeshX = ball.meshX;
+    ball.baseMeshY = ball.meshY;
+    ball.baseMeshZ = ball.meshZ;
+    
+    ball.meshX = ball.baseMeshX + ball.posX;
+    ball.meshY = ball.baseMeshY + ball.posY;
+    ball.meshZ = ball.baseMeshZ + ball.posZ;
+    
     % Azimuthal angle
     az = maxAz * (networkOutput(4) - 0.5);
-    if player == 2
+    if player == 1
         az = az + pi;
     end
     
@@ -39,4 +60,7 @@ function ball = createBall(networkOutput,court,player)
     ball.velX = vXY * cos(az);
     ball.velY = vXY * sin(az);
     ball.velZ = v0 * sin(el);
+    
+    % Draw
+    ball.handler = surf(ball.meshX,ball.meshY,ball.meshZ,'FaceColor',ball.color,'EdgeAlpha',ball.edgeAlpha);
 end
