89700a3aece84e41175e496cc12caa5832a4f07f
jmontemayorm <j.montemayor.mm@gmail.com>
2019-05-14 09:28:47 -0500

    Added variations, propagation, and extrapolation

--- shootBall.m
+++ shootBall.m
@@ -13,6 +13,28 @@ function court = shootBall(neuralNetwork,environment,court,player)
         outData = neuralNetwork.sigmoidF(outData);
     end
     
-    disp(outData)
+    % Add random variations
+    variations = 0.15 .* rand(6,1);
+    signs = 2 .* randi([0,1],[6,1]) - 1;
+    outData = signs .* variations + outData;
+    outData(outData > 1) = 1;
+    outData(outData < 0) = 0;
+    
+    % Create ball
+    ball = createBall(outData);
+    
+    % Get XY plane height (cup entrance)
+    xy_plane = court.table.bottomHeight + court.table.thickness + court.cups.floatingSpace + court.cups.height;
+    
+    % Loop while the ball is going upward or above the plane
+    while (ball.posZ > xy_plane || ball.velZ >= 0)
+        oldBall = ball;
+        ball = propagateBall(ball,environment);
+    end
+    
+    % Extrapolate to the XY plane
+    delta_t = (xy_plane - oldBall.posZ) ./ oldBall.velZ;
+    ballXY(1) = oldBall.posX + oldBall.velX * delta_t;
+    ballXY(2) = oldBall.posY + oldBall.velY * delta_t;
 end
 
