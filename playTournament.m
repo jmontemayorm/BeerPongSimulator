0d2e5423a2c224928618a3d1e3dc66d599749e55
jmontemayorm <j.montemayor.mm@gmail.com>
2019-05-14 18:12:15 -0500

    Fixed playTournament

--- playTournament.m
+++ playTournament.m
@@ -1,59 +1,57 @@
-function Ranking = playTournament(Players)
-
-populationSize = length(Players);
-
-%Initial values of tournament
-Shots = 10;
-n = 4;
-populationSize = 2^n;
-NMatches = populationSize/2;
-
-
-% % %Create players
-% % Players = cell(populationSize,1);
-% % for NewPlayer = 1:populationSize
-% %     Players{NewPlayer} = createNeuralNetwork();
-% % end
-
-
-% Tournament preparations
-Advances = cell(n+1,1);
-for Ad = 1:n+1
-    Advances{Ad} = zeros(2^(n+1-Ad),1);
-end
-Advances{1} = 1:populationSize;
-
-Ranking = zeros(populationSize,1);
-count = 1;
-
-
-
-%Face off
-for Rounds = 1:n
+function ranking = playTournament(players,maxShotsPerPlayer)
+    % TODO: Add description
     
-    Round = reshape(randperm(NMatches*2)',2,NMatches,[])';
+    % Extract population size
+    populationSize = length(players);
+
+    % Default maximum number of shots per player
+    if nargin < 2
+        maxShotsPerPlayer = 50;
+    end
     
-    for Matches = 1:NMatches
-        
-        Red1 = Players{Advances{n}(Round(n,1))};
-        Blue2 = Players{Advances{n}(Round(n,2))};
-        [Redscore1,Bluescore2] = beerpongMatch(Red1,Blue2,Shots,false);
+    numOfRounds = log2(populationSize);
+
+    % Allocate tournament round places
+    previousRoundVictories = cell(numOfRounds+1,1);
+    for prv = 1:(numOfRounds + 1)
+        previousRoundVictories{prv} = zeros(2^(numOfRounds + 1 - prv),1);
+    end
     
-        if Redscore1 >= Bluescore2
-            Advances{n+1}(NMatches) = (Advances{n}(Round(n,1)); %Red1 Advances
-            Ranking(count) = (Advances{n}(Round(n,2)); %Blue2 Losses
-        else
-           Advances{n+1}(NMatches) = (Advances{n}(Round(n,2)); %Blue2 Advances
-           Ranking(count) = (Advances{n}(Round(n,2)); %Red1 Losses
+    % All players go into the first round
+    previousRoundVictories{1} = 1:populationSize;
+
+    % Allocate for ranking
+    ranking = zeros(populationSize,1);
+    count = 1;
+
+    numOfMatches = populationSize/2;
+    
+    % Play tournament
+    for round = 1:numOfRounds
+        matchOrder = reshape(randperm(numOfMatches*2)',2,numOfMatches,[])';
+
+        for match = 1:numOfMatches
+            home = players{previousRoundVictories{round}(matchOrder(match,1))};
+            visitor = players{previousRoundVictories{round}(matchOrder(match,2))};
+            [homeScore,visitorScore] = beerpongMatch(home,visitor,maxShotsPerPlayer,false);
+
+            if homeScore >= visitorScore
+                previousRoundVictories{round+1}(match) = previousRoundVictories{round}(matchOrder(match,1));
+                ranking(count) = previousRoundVictories{round}(matchOrder(match,2));
+            else
+               previousRoundVictories{round+1}(match) = previousRoundVictories{round}(matchOrder(match,2));
+               ranking(count) = previousRoundVictories{round}(matchOrder(match,1));
+            end
+
+            count = count + 1;
         end
         
-        count = count + 1;
+        numOfMatches = numOfMatches/2; 
     end
-    NMatches = NMatches/2; 
-end
+    
+    % Add winner to the ranking
+    ranking(count) = previousRoundVictories{numOfRounds + 1};
 
-
-%Order Raking in right order
-Ranking = flip(Ranking);
-
+    % Order Raking in right order
+    ranking = flip(ranking);
 end
