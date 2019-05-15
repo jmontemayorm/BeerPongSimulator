0594707f85d0037f812d9f91dca481e71b94d7a1
jmontemayorm <j.montemayor.mm@gmail.com>
2019-05-14 19:25:21 -0500

    main and minor changes

--- beerpong.m
+++ beerpong.m
@@ -7,7 +7,7 @@ maxGenerations = 5000;
 
 % Timeout
 enableTimeout = 1;
-timeoutMinutes = 1;
+timeoutMinutes = 20;
 
 % Console output
 suppressOutput = 0;
@@ -21,19 +21,22 @@ elitismFraction = 0.1;
 % Population
 populationSize = 2^7; % Must be a power of 2, because of the tournament
 
+maxShotsPerPlayer = 20;
+
 paternalProbability = 0.6;
 
 % Mutation (0 = static, 1 = linear, 2 = exponential, 3 = cyclical)
-dynamicMutation = 3;
+% dynamicMutation = 3;
 
-staticMutationProbability = 0.000001;
+mutationProbability = 0.000001;
+maxMutationSize = 0.1;
 
-initialMutationProbability = 0.00005;
-finalMutationProbability = 0.0000005;
-
-finalDynamicGeneration = 30000;
-
-cyclicalWavelengthInGenerations = 150;
+% initialMutationProbability = 0.00005;
+% finalMutationProbability = 0.0000005;
+% 
+% finalDynamicGeneration = 30000;
+% 
+% cyclicalWavelengthInGenerations = 150;
 
 % Cooldown
 enableCooldown = 0;
@@ -52,8 +55,8 @@ eliteAmount = floor(populationSize * elitismFraction);
 nonEliteIdx = (eliteAmount + 1):populationSize;
 
 % Mutation
-eta = (finalMutationProbability - initialMutationProbability) / finalDynamicGeneration;  
-alpha = (finalMutationProbability / initialMutationProbability) ^ (1 / finalDynamicGeneration);
+% eta = (finalMutationProbability - initialMutationProbability) / finalDynamicGeneration;  
+% alpha = (finalMutationProbability / initialMutationProbability) ^ (1 / finalDynamicGeneration);
 
 %% Initial population
 players = cell(populationSize,1);
@@ -62,13 +65,15 @@ for p = 1:populationSize
     players{p} = createNeuralNetwork;
 end
 
+exampleNN = players{1};
+
 %% Evolution
 generation = 1;
 
 tic
 while true % Breaking conditions found before updating the counter
     
-    %fitness = tournament(); 
+    ranking = playTournament(players,maxShotsPerPlayer);
     
     % % % Survival of the fittest % % %
     % Acquire targets
@@ -76,12 +81,12 @@ while true % Breaking conditions found before updating the counter
     killIdx = false(1,populationSize);
     while killed < populationSize / 2
         % Go from unfittest to fittest
-        unfitToFit = flip(bestIdx(nonEliteIdx)); % Only the non elite
+        unfitToFit = flip(ranking(nonEliteIdx)); % Only the non elite
         
         for p = 1:(populationSize - eliteAmount)
             player = unfitToFit(p);
             % Always include probability of survival (also for unfittest, elitism exception)
-            if (killIdx(player) == false) && (exp(find(player == bestIdx,1)/populationSize - 1.1) >= rand)
+            if (killIdx(player) == false) && (exp(find(player == ranking,1)/populationSize - 1.1) >= rand)
                 % Acquire target
                 killIdx(player) = true;
                 
@@ -112,7 +117,7 @@ while true % Breaking conditions found before updating the counter
         lookingForFirstParent = true;
         while lookingForFirstParent
             % The most fit are the firsts in line
-            orderedCandidatesIdx = bestIdx(allowedIdx);
+            orderedCandidatesIdx = ranking(allowedIdx);
             
             for candidate = 1:length(orderedCandidatesIdx)                
                 if rand < paternalProbability
@@ -129,7 +134,7 @@ while true % Breaking conditions found before updating the counter
         lookingForSecondParent = true;
         while lookingForSecondParent
             % The most fit are the firsts in line
-            orderedCandidatesIdx = bestIdx(allowedIdx);
+            orderedCandidatesIdx = ranking(allowedIdx);
             
             for candidate = 1:length(orderedCandidatesIdx)
                 if rand < paternalProbability
@@ -140,49 +145,43 @@ while true % Breaking conditions found before updating the counter
             end
         end
         
-        % TODO: ADAPT
         % Make baby
-        for layer = 1:length()
-        end
-        firstParent = randi([0, 1], [numOfPolygons, 1]);
-        secondParent = ~firstParent;
-        players{replaceWithBaby(newBaby)} = firstParent .* players{firstParentIdx} + secondParent .* players{secondParentIdx};
-        
-        
-        % EXAMPLE
-        for layer = 1:length(nn.layers)
-            mutate = rand(size(nn.layers{layer})) < mutationProbability;
-            sign = 2 * randi([0,1],size(nn.layers{layer})) - 1;
-            mutation = maxMutationSize .* rand(size(nn.layers{layer}));
-
-            nn.layers{layer}(mutate) = nn.layers{layer}(mutate) + sign(mutate) .* mutation(mutate);
-            nn.layers{layer}(nn.layers{layer} < -1) = -1;
-            nn.layers{layer}(nn.layers{layer} > 1) = 1;
+        for layer = 1:length(exampleNN.layers)
+            firstParent = randi([0, 1], size(exampleNN.layers{layer}));
+            secondParent = ~firstParent;
+            players{replaceWithBaby(newBaby)}.layers{layer} = firstParent .* players{firstParentIdx}.layers{layer} + secondParent .* players{secondParentIdx}.layers{layer};
         end
     end
     
     % % % Mutations % % %
     % Stick to a static mutation if dynamic has ended
-    if generation > finalDynamicGeneration
-        dynamicMutation = 0;
-    end
+%     if generation > finalDynamicGeneration
+%         dynamicMutation = 0;
+%     end
 
     % Calculate the respective mutation probability
-    if dynamicMutation == 1
-        mutationProbability = initialMutationProbability - eta * generation;
-    elseif dynamicMutation == 2
-        mutationProbability = initialMutationProbability * alpha ^ generation;
-    elseif dynamicMutation == 3
-        normalizedCosine = 0.5 * (1 + cos(2 * pi * generation / cyclicalWavelengthInGenerations));
-        mutationProbability = finalMutationProbability + (initialMutationProbability - finalMutationProbability) * normalizedCosine;
-    else
-        mutationProbability = staticMutationProbability;
-    end
+%     if dynamicMutation == 1
+%         mutationProbability = initialMutationProbability - eta * generation;
+%     elseif dynamicMutation == 2
+%         mutationProbability = initialMutationProbability * alpha ^ generation;
+%     elseif dynamicMutation == 3
+%         normalizedCosine = 0.5 * (1 + cos(2 * pi * generation / cyclicalWavelengthInGenerations));
+%         mutationProbability = finalMutationProbability + (initialMutationProbability - finalMutationProbability) * normalizedCosine;
+%     else
+%         mutationProbability = staticMutationProbability;
+%     end
     
     % Mutate
     for player = 1:populationSize
-        mutate = rand(numOfPolygons, geneSize) < mutationProbability;
-        players{player}(mutate) = ~players{player}(mutate);
+        for layer = 1:length(exampleNN.layers)
+            mutate = rand(size(exampleNN.layers{layer})) < mutationProbability;
+            sign = 2 * randi([0,1],size(exampleNN.layers{layer})) - 1;
+            mutation = maxMutationSize .* rand(size(exampleNN.layers{layer}));
+
+            players{player}.layers{layer}(mutate) = players{player}.layers{layer}(mutate) + sign(mutate) .* mutation(mutate);
+            players{player}.layers{layer}(players{player}.layers{layer} < -1) = -1;
+            players{player}.layers{layer}(players{player}.layers{layer} > 1) = 1;
+        end
     end
     
     % % % Breaking mechanisms % % %
